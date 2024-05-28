58318b5331482d40c7e9df90e533dcd168afb06b
Rukaiya <rukaiyakhan96@gmail.com>
2024-05-28 21:29:07 +0530

    Integers tutorial

---
+++ Python functions/integers,float.py
@@ -0,0 +1,29 @@
+number = 7
+print(type(number))
+# Addition 
+print(6 + 7)
+#substraction
+print(96 - 66)
+# Multiplication 
+print(44 * 5)
+# Division 
+print(78 / 12)
+#Floor division
+print(78 // 11)
+# Modulus 
+print(78 % 4)
+#Mixed calculations 
+print(number * (76 - 43))
+
+##incrementing values
+#method 1
+number = number + 6
+print(number)
+# method 2
+number += 6
+print(number)
+ # absolute
+print(abs(-3.5))
+print(abs(-6))
+#round to the number of digits
+print(round(5.05956969, 3))
