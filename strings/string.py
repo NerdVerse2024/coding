e86ff98d6682e88f7df2b2c6b23787d674ac4e26
Rukaiya <rukaiyakhan96@gmail.com>
2024-05-28 20:43:59 +0530

    first commit

---
+++ strings/string.py
@@ -0,0 +1,17 @@
+print("Hello world")
+print('This is Rukaiya')
+print("""I am determined to 
+become a bioinformatician no matter what it takes.""")
+new_message = "Python, Galaxy Server, Blast Tools"
+print("I have got to learn ", new_message , "initially.")
+print(len(new_message))
+print(new_message[0])
+print(new_message[1:15])
+print(new_message.count('t'))
+print(new_message.find('x'))
+new_message = new_message.replace('Galaxy Server', 'GProfiler')
+print(new_message)
+course = 'Galaxy Server'
+course1 = 'GitHub'
+latest_message = f'{course}, {course1.upper()}. plus these two!'
+print(latest_message)
